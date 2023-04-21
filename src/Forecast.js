bd99c8bd57be7073f85a0f389adb2c53845fb83a
ZuzanaZitkova <suegaldova@gmail.com>
2023-04-21 13:03:50 +1200

    changed files with style .css conected together to one app.css

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './styles.css';
+import './App.css';
 
 export default function Weather() {
   let [city, setCity] = useState('');
